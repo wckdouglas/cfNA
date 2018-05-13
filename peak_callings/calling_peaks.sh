0942661e7481fa2138ba96a923fce164c89e8335
wckdouglas <wckdouglas@gmail.com>
2018-05-13 13:05:46 -0500

    added script

---
+++ peak_callings/calling_peaks.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+PROJECT_PATH=$SCRATCH/cell_Free_nucleotides/tgirt_map
+WPS_PATH=$PROJECT_PATH/merged_bed/genome_WPS
+PEAK_PATH=$WPS_PATH/genome_peaks
+mkdir -p $PEAK_PATH
+
+for WPS in $WPS_PATH/*bigWig
+do
+	SAMPLENAME=$(basename $WPS)
+	PEAK_FILE=$PEAK_PATH/${SAMPLENAME/.bigWig/.bed}
+	STRAND=$(echo $SAMPLENAME | rev |cut -d'.' -f2 |rev)
+
+    STRANDENESS=''
+    if [[ $STRAND == "fwd" ]]
+    then
+        STRANDENESS='forward'
+    elif [[ $STRAND == "rvs" ]]
+    then
+        STRANDENESS='reverse'
+    fi
+
+	echo python peak_caller.py \
+		--in_bigwig $WPS  \
+		--out_bed  $PEAK_FILE \
+		--length_type Short \
+		--strand $STRANDENESS \
+        --two_pass 
+done

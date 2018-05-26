1c0acb671f8a4acb322e4507eed3f1c93c75dc74
wckdouglas <wckdouglas@gmail.com>
2018-05-26 13:24:03 -0500

    added train

---
+++ dna_rna_classifier/train.sh
@@ -0,0 +1,10 @@
+WORK_DIR=/stor/work/Lambowitz/cdw2854/cell_Free_nucleotides/tgirt_map/classifier/
+
+deep-cfNA train \
+    --train_bed_positive $WORK_DIR/train_RNA.bed \
+    --train_bed_negative $WORK_DIR/train_DNA.bed \
+    --validation_bed $WORK_DIR/test.bed \
+    --genome $REF/hg19/genome/hg19_genome.fa \
+    --model_prefix $WORK_DIR/deep_cfNA \
+    --epoch 5
+

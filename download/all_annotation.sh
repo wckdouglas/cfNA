45b3b121ee63bbfa87b3b00dd26766c2d99cf3af
wckdouglas <wckdouglas@gmail.com>
2018-06-01 11:38:44 -0500

    updated

--- download/all_annotation.sh
+++ download/all_annotation.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-REF_PATH=$REF/hg19/genome
+REF_PATH=$REF/hg19
+GENOME_PATH=$REF_PATH/genome
+GENE_PATH=$REF_PATH/new_genes
 
-cat $REF_PATH/RBP.bed \
+cat $GENOME_PATH/RBP.bed \
 | grep -v HepG2 \
 | tr '_' '\t' \
 | awk '{print $1,$2,$3,$4,$7,$8, "RBP",$4}' OFS='\t' \
-| cat - $REF_PATH/genes.bed $REF_PATH/rmsk.bed $REF_PATH/hg19_refseq.bed \
+| cat - $GENE_PATH/genes.bed $GENOME_PATH/rmsk.bed $GENOME_PATH/hg19_refseq.bed \
 | sort -k1,1 -k2,2n -k3,3n \
 | bgzip \
-> $REF_PATH/all_annotation.bed.gz
+> $GENE_PATH/all_annotation.bed.gz
